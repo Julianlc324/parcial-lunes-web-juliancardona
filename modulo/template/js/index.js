ffa62d0769854c6f1a1e7e480056e3f83f7a9a17
Julianlc324 <31931760+Julianlc324@users.noreply.github.com>
2025-03-30 15:41:40 -0500

    Se realiza la primera parte del index.html Se realiza la maquetacion css, grid, flexbox se realiza el consumo de la API en el index.html

---
+++ modulo/template/js/index.js
@@ -0,0 +1,23 @@
+/* Pokemon iniciales */
+const baseURL = 'https://pokeapi.co'
+const endpoint = '/api/v2/pokemon/'
+const url = `${baseURL}${endpoint}`
+
+let contadorEtiqueta = 1;
+const cargarPokemonInicial = async () => {
+  const pokemonIds = [1,4,7,152,155,158, 252, 255, 258]
+  for(let id of pokemonIds){
+    const respuesta = await fetch(`${url}${id}`)
+    const dato = await respuesta.json()
+    let etiquetaClase = document.getElementsByClassName(`grid_article${contadorEtiqueta}`)
+    etiquetaClase[0].innerHTML = `
+      <figure>
+        <img src=/assets/img/pokemon/${dato.id}.png alt=${dato.name}>
+        <figcaption class=flex_subtitulo> ${dato.name} </figcaption>
+      </figure>
+    `
+    contadorEtiqueta++
+  }
+}
+//Llamar funcion cuando cargue pagina
+window.onload = cargarPokemonInicial()
