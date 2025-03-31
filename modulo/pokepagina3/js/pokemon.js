4565357d0c815a3f13aa4fa7266aa135b78dbabb
Julianlc324 <31931760+Julianlc324@users.noreply.github.com>
2025-03-31 16:17:30 -0500

    Se realiza cambios en la 3 opcion del menu Se ajusta la API para la busquedad de los pokemon

---
+++ modulo/pokepagina3/js/pokemon.js
@@ -0,0 +1,46 @@
+/* Pokemon iniciales */
+const baseURL = 'https://pokeapi.co'
+const endpoint = '/api/v2/pokemon/'
+const url = `${baseURL}${endpoint}`
+
+/* Buscador pokémon */
+const obtenerPokemon =  () => {
+	let obtenerCampo = document.getElementById('busca_pokemon')
+	let valor = obtenerCampo.value
+	buscaTuPokemon(valor)
+}
+
+const buscaTuPokemon = async (pokemon) => {
+  const respuesta = await fetch(`${url}${pokemon}`)
+  const dato = await respuesta.json()
+	let etiquetaClase = document.getElementsByClassName(`article_buscar`)
+	etiquetaClase[0].innerHTML = `
+		<figure class=cajon_pokemon>
+			<img src=/assets/img/pokemon/${dato.id}.png alt=${dato.name}>
+		</figure>
+		<div class=cajon_dato> 
+			<h3> Numero ID: ${dato.id} </h3> 
+			<h3> Nombre: ${dato.name} </h3>
+			<h3> Altura: ${(dato.height)/10} </h3>
+			<h3> Peso: ${(dato.weight)/10} </h3>
+		</div>	
+	`
+}
+
+/* Carga los pokémon */
+const cargarPokemonGeneracion3 = async () => {
+  for(let i=252; i<=386; i++){
+    const respuesta = await fetch(`${url}${i}`)
+    const dato = await respuesta.json()
+    let etiquetaClase = document.getElementsByClassName(`article${i}`)
+    etiquetaClase[0].innerHTML = `
+      <figure>
+        <img src=/assets/img/pokemon/${dato.id}.png alt=${dato.name}>
+        <h3 class=flex_subtitulo> Nº pokémon ${dato.id} </h3>
+				<h3 class=flex_subtitulo > ${dato.name} </h3>
+      </figure>
+    `
+  }
+}
+//Llamar funcion cuando cargue pagina
+window.onload = cargarPokemonGeneracion3()
